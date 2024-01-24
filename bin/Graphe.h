c285962748b9869610e976d3a188417ee2a0a639
Djalil Chikhi <djalilchiki@gmail.com>
2024-01-24 14:42:22 +0100

    grahe.h

---
+++ bin/Graphe.h
@@ -0,0 +1,39 @@
+/*************************************************************************
+                           Graphe  -  description
+                             -------------------
+    début                : 15/01/2021
+    copyright            : (C) 2021 by Chikhi Djalil & Thabet Yasmine  
+                            & Cavagna Justine  & Hanader Rayan 
+*************************************************************************/
+
+//---------- Implémentation de la classe <Graphe> (fichier Graphe.h) --
+#if ! defined (GRAPHE_H)
+#define GRAPHE_H
+
+#include <map>
+using namespace std;
+#include "Noeud.h"
+
+class Graphe
+{
+//----------------------------------------------------------------- PUBLIC
+public:
+//----------------------------------------------------- Méthodes publiques
+//-------------------------------------------- Constructeur - destructeur
+    Graphe();
+
+    virtual ~Graphe();
+    const map < string, Noeud > & obtenirTableLiens();
+
+    bool ajouterLien(string nomNoeud1, string nomNoeud2, bool condition = true);
+
+    const vector < Noeud > obtenirNoeudsPlusConnectes(int n) const;
+
+//------------------------------------------------------------------ PRIVATE 
+//----------------------------------------------------- Attributs protégés
+protected:
+    map < string, Noeud > tableLiens;
+    unsigned int nombreNoeuds;
+};
+
+#endif // GRAPHE_H
